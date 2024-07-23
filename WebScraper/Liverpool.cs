62ac9e7c135e81c5614031f68032f8dec8dad4fb
Alberto Najera Dominguez <elblogdelbeto@outlook.com>
2024-07-22 22:46:05 -0600

    primero commit con el obteniendo el htmlWeb funcionando
    tuve que usar este valor porque sino Liverpool rechazaba la conexion con un error de  unautorized:
    htmlWeb.UserAgent = "Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148";

---
+++ WebScraper/Liverpool.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace WebScraper
+{
+    internal class Liverpool
+    {
+        static internal void ObtenerNintendos()
+        {
+            string url = "https://www.liverpool.com.mx/tienda/consolas-nintendo/catst16854843";
+
+            HtmlDocument htmlDocument = new HtmlDocument();
+            HtmlWeb htmlWeb = new HtmlWeb();
+
+            htmlWeb.OverrideEncoding = Encoding.UTF8;
+            htmlWeb.UserAgent = "Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148";
+            htmlDocument = htmlWeb.Load(url);
+            var tituloElemento = htmlDocument.DocumentNode.SelectSingleNode(@"//script[@id=""__NEXT_DATA__""]/text()");
+
+            string outerHtml = htmlDocument.DocumentNode.OuterHtml;
+
+            HttpClient httpClient = new HttpClient();
+            string htmlString = httpClient.GetStringAsync(url).Result;
+            htmlDocument.LoadHtml(htmlString);
+
+            tituloElemento = htmlDocument.DocumentNode.SelectSingleNode(@"//h5[@class='card-title a-card-description']/span[@class='m-description-spcbrand']/parent::h5");
+            var titulo = tituloElemento.InnerText.Trim();
+
+        }
+         
+    }
+}
---
+++ WebScraper/Program.cs
@@ -0,0 +1,8 @@
+﻿using WebScraper;
+
+
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Liverpool.ObtenerNintendos();
